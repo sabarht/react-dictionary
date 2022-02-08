b03d588a0d11efff35f8f836b141bcb8fadfb558
sabarht <sabarht@gmail.com>
2022-02-08 10:44:29 +0100

    adding form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <h1>Dictionary Project</h1>
       </header>
+
+      <main>
+        <Dictionary />
+      </main>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+  }
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={updateKeyword} />
+        <input type="submit" value="search" />
+      </form>
+    </div>
+  );
+}
